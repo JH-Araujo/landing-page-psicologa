e4f343f6f2e02a8d4182c0dd4e3dd454e4b3adc9
João Henrique <kj.henrique@gmail.com>
2024-04-07 22:12:48 -0300

    Function closeMenu()

--- Js/menu.js
+++ Js/menu.js
@@ -1,5 +1,6 @@
 var menuButton = document.getElementById("menu-button");
 var menu = document.getElementById("menu");
+var optionButton = document.getElementsByClassName("options")
 
 menuButton.addEventListener("click", function() {
   if (menu.style.display === "block") {
@@ -8,3 +9,11 @@ menuButton.addEventListener("click", function() {
     menu.style.display = "block";
   }
 });
+
+function closeMenu(){
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
